1fcb936c3fdb3531b670b4d80dbc960ce1411d73
MaiRizk <mai.riizk@gmail.com>
2022-01-10 23:25:48 +0200

    Update date.sql with the requirments

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
 /* Database schema to keep the structure of entire database. */
 
 createdb vet_clinic;
-CREATE TABLE animals(id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, name TEXT, date_of_birth DATE, escape_attempts INT, neutered BOOLEAN, weight_kg DECIMAL);
+CREATE TABLE animals(id INT GENERATED ALWAYS AS IDENTITY, name TEXT, date_of_birth DATE, escape_attempts INT, neutered BOOLEAN, weight_kg DECIMAL);
