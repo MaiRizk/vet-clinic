f102c051adcb3641f95f857744ae7a04f4e4f3f7
MaiRizk <mai.riizk@gmail.com>
2022-01-13 09:36:30 +0200

    Remove all comments

--- data.sql
+++ data.sql
@@ -1,12 +1,8 @@
-/* Populate database with sample data. */
-
-/* Insert values to animals table (Milestone 1) */
 INSERT INTO animals(name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Agumon', '2020-02-03', 10.23, true, 0);
 INSERT INTO animals(name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Gabumon', '2018-11-15', 8, true, 2);
 INSERT INTO animals(name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Pikachu', '2021-01-07', 15.04, false, 1);
 INSERT INTO animals(name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Devimon', '2017-05-12', 11, true, 5);
 
-/* Insert new values to animals table (Milestone 2) */
 INSERT INTO animals(name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Charmander', '2020-02-08', '-11', false, 0);
 INSERT INTO animals(name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Plantmon', '2022-11-15', '-5.7', true, 2);
 INSERT INTO animals(name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Squirtle', '1993-04-02', '-12.13', false, 3);
@@ -14,9 +10,6 @@ INSERT INTO animals(name, date_of_birth, weight_kg, neutered, escape_attempts) V
 INSERT INTO animals(name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Boarmon', '2005-06-07', '20.4', true, 7);
 INSERT INTO animals(name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES ('Blossom', '1998-10-13', '17', true
 
-/* New insertions (Milestone 3) */
-
-/* Insert values into orders table */
 INSERT INTO owners(full_name, age) VALUES ('Sam Smith', 34);
 INSERT INTO owners(full_name, age) VALUES ('Jennifer Orwell', 19);
 INSERT INTO owners(full_name, age) VALUES ('Bob', 45);
@@ -24,7 +17,6 @@ INSERT INTO owners(full_name, age) VALUES ('Melody Pond', 77);
 INSERT INTO owners(full_name, age) VALUES ('Dean Winchester', 14);
 INSERT INTO owners(full_name, age) VALUES ('Jodie Whittaker', 38);
 
-/* Insert values into species table */
 INSERT INTO species(name) VALUES ('Pokemon');
 INSERT INTO species(name) VALUES ('Digimon');
 
--- schema.sql
+++ schema.sql
@@ -1,23 +1,14 @@
-/* Database schema to keep the structure of entire database. */
-
-/* Create database and animals table (Milestone 1) */
 createdb vet_clinic;
 CREATE TABLE animals(id INT GENERATED ALWAYS AS IDENTITY, name TEXT, date_of_birth DATE, escape_attempts INT, neutered BOOLEAN, weight_kg DECIMAL);
 
-/* Create species column (Milestone 2) */
 ALTER TABLE animals ADD COLUMN species TEXT;
 
-/* Milestone 3 */
-
-/* Create table owners */
 DROP TABLE IF EXISTS owners; 
 CREATE TABLE owners(id INT GENERATED ALWAYS AS IDENTITY, full_name TEXT, age INT, PRIMARY KEY (id));
 
-/* Create table species */
 DROP TABLE IF EXISTS species;
 CREATE TABLE species(id INT GENERATED ALWAYS AS IDENTITY, name TEXT, PRIMARY KEY(id));
 
-/* Update animals table */
 ALTER TABLE animals ADD PRIMARY KEY (id);
 ALTER TABLE animals DROP COLUMN species;
 ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id);
