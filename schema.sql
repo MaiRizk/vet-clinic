0921244658a5283ec4951ea987c5c9aa69b14b5b
MaiRizk <mai.riizk@gmail.com>
2022-01-13 03:12:50 +0200

    Update schema.sql with the required changes

--- schema.sql
+++ schema.sql
@@ -6,3 +6,21 @@ CREATE TABLE animals(id INT GENERATED ALWAYS AS IDENTITY, name TEXT, date_of_bir
 
 /* Create species column (Milestone 2) */
 ALTER TABLE animals ADD COLUMN species TEXT;
+
+/* Milestone 3 */
+
+/* Create table owners */
+DROP TABLE IF EXISTS owners; 
+CREATE TABLE owners(id INT GENERATED ALWAYS AS IDENTITY, full_name TEXT, age INT, PRIMARY KEY (id));
+
+/* Create table species */
+DROP TABLE IF EXISTS species;
+CREATE TABLE species(id INT GENERATED ALWAYS AS IDENTITY, name TEXT, PRIMARY KEY(id));
+
+/* Update animals table */
+ALTER TABLE animals ADD PRIMARY KEY (id);
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id);
+ALTER TABLE animals ADD CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species(id) ON DELETE CASCADE;
+ALTER TABLE animals ADD COLUMN owners_id INT REFERENCES owners(id);
+ALTER TABLE animals ADD CONSTRAINT fk_owners FOREIGN KEY (owners_id) REFERENCES owners(id) ON DELETE CASCADE;
